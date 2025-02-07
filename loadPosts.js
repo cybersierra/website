3748b5310a105637b2d8cde332bebc6ad698db2c
cybersierra <156851320+cybersierra@users.noreply.github.com>
2025-02-07 02:01:51 -0600

    created new 'recent posts' for homepage

---
+++ loadPosts.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+    fetch("posts.json") // Fetch the JSON file
+        .then(response => response.json())
+        .then(posts => {
+            let container = document.getElementById("blog-snippets");
+
+            posts.forEach(post => {
+                let postHTML = `
+                    <article class="blog-snippet">
+                        <h3><a href="${post.link}">${post.title}</a></h3>
+                        <p><em>${post.date}</em></p>
+                        <p>${post.snippet} <a href="${post.link}" class="read-more">Read More</a></p>
+                    </article>
+                `;
+                container.innerHTML += postHTML;
+            });
+        })
+        .catch(error => console.error("Error loading posts:", error));
+});
